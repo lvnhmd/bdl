e134df15534fb3989233e4f95470ddfdade4c5fb
elvin <ali.elvin@gmail.com>
2015-08-16 21:11:42 +0100

    add config

---
+++ config.js
@@ -0,0 +1,13 @@
+/*jslint node: true */
+'use strict';
+module.exports = {
+    port: 8080,
+    // source configs
+    sourceTypes: {
+        'dynamic': {
+            method: 'GET',
+            basePath: 'http://',
+            feed: 'dynamic'
+        }
+    }
+};
